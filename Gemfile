bfb04cd2b2610b6a1414ef578a0fd604129b424d
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-25 13:32:38 +0000

    fixed api call

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem 'pry'
+gem 'open_uri'
 
 # Specify your gem's dependencies in cli_project.gemspec
 gemspec
--- lib/marta_api_importer.rb
+++ lib/marta_api_importer.rb
@@ -2,20 +2,25 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 require 'pry'
+require 'addressable/uri'
 
 class MartaTrainScheduleImporter 
-  #the url to receive train schedule information from 
-  url = 'http://developer.itsmarta.com/RealtimeTrain/
-RestServiceNextTrain/GetRealtimeArrivals?apikey='
-
-  #store the url as a class object
-  uri = URI.parse(url)
+  @@api_key = ''
+  def self.train_api_call(api_key)
+    #the url to receive train schedule information from 
+    url = "http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=#{api_key}"
   
-  #sending a get request to return a value of Net::HTTPOK object 
-  responsed = Net::HTTP.get_response(uri)
+    #store the url as a class object
+    uri = URI.parse(URI.encode(url))
+    
+    # #sending a get request to return a value of Net::HTTPOK object 
+    responsed = Net::HTTP.get_response(uri)
   
-  #parse using json
-  hash = JSON.parse(responsed.body)
+    # #parse using json
+    train_array = JSON.parse(responsed.body)
   
-  binding.pry
+    binding.pry
+  end 
 end 
+
+MartaTrainScheduleImporter.train_api_call('')
